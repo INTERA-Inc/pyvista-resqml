0d4e7d8fe78c251015bcbd917fbd663a412a13a7
Keurfon Luu <keurfonluu@outlook.com>
2023-12-14 13:58:29 +0100

    fix face order of pyramid

--- meshio_resqml/_write.py
+++ meshio_resqml/_write.py
@@ -17,8 +17,8 @@ meshio_type_to_faces = {
         "triangle": np.array([[1, 2, 3], [0, 3, 2], [0, 1, 3], [0, 2, 1]]),
     },
     "pyramid": {
-        "triangle": np.array([[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]]),
         "quad": np.array([[0, 3, 2, 1]]),
+        "triangle": np.array([[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]]),
     },
     "wedge": {
         "triangle": np.array([[0, 2, 1], [3, 4, 5]]),
