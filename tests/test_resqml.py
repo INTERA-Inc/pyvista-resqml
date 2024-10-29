5ace01e1cb8494367de6172e8b31047201fb902f
Keurfon Luu <keurfonluu@outlook.com>
2024-10-29 15:38:17 -0500

    update tests

--- tests/test_resqml.py
+++ tests/test_resqml.py
@@ -3,19 +3,18 @@ import pathlib
 import helpers
 import pytest
 
-import meshio_resqml
+import pvresqml
 
 
 @pytest.mark.parametrize(
     "mesh",
     [
-        helpers.tetra_mesh,
-        helpers.pyramid_mesh,
-        helpers.wedge_mesh,
-        helpers.hexahedron_mesh,
-        helpers.hybrid_mesh,
-        helpers.poly_hybrid_mesh,
-        helpers.polyhedron_mesh,
+        pvresqml.examples.load_tetra_mesh(),
+        pvresqml.examples.load_pyramid_mesh(),
+        pvresqml.examples.load_wedge_mesh(),
+        pvresqml.examples.load_hexahedron_mesh(),
+        pvresqml.examples.load_hybrid_mesh(),
+        pvresqml.examples.load_polyhedron_mesh(),
         "block.epc",
         "s_bend.epc",
     ],
@@ -24,6 +23,6 @@ def test_mesh(mesh, tmp_path):
     if isinstance(mesh, str):
         this_dir = pathlib.Path(__file__).resolve().parent
         filename = this_dir / "support_files" / mesh
-        mesh = meshio_resqml.read(filename)
+        mesh = pvresqml.read(filename).cast_to_unstructured_grid()
 
-    helpers.write_read(tmp_path, meshio_resqml.write, meshio_resqml.read, mesh, 1.0e-15)
+    helpers.write_read(tmp_path, pvresqml.save, pvresqml.read, mesh, 1.0e-15)
