5019c69d05ddc922df0895bba105429cc8452477
Keurfon Luu <keurfonluu@outlook.com>
2026-05-18 11:59:37 +0200

    modernize

--- pvresqml/__init__.py
+++ pvresqml/__init__.py
@@ -1,9 +1,9 @@
 """RESQML extension for PyVista."""
 
-from . import examples
-from .__about__ import __version__
-from ._read import read
-from ._save import save
+from . import examples as examples
+from .__about__ import __version__ as __version__
+from ._read import read as read
+from ._save import save as save
 
 
 __all__ = [x for x in dir() if not x.startswith("_")]  # type: ignore
